87474b55af163b7fe8e641ff4be185b98dd9e56b
piyush kumar sadangi <piyushkumarsadangi@MacBook-Air.local>
2024-10-10 03:53:31 +0530

    testYML

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/MethodNameCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/MethodNameCheck.java
@@ -103,6 +103,7 @@ public class MethodNameCheck
      */
     private boolean allowClassName;
 
+
     /** Creates a new {@code MethodNameCheck} instance. */
     public MethodNameCheck() {
         super("^[a-z][a-zA-Z0-9]*$");
